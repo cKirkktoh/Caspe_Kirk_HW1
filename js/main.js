0f6a0a853fd91d92e620ba3b8a6e04b2a7a1cfa7
cKirkktoh <113542793+cKirkktoh@users.noreply.github.com>
2024-01-29 23:10:22 -0500

    added images and more codes

--- js/main.js
+++ js/main.js
@@ -1,69 +1,66 @@
 (() => {
     const movieBox = document.querySelector("#movie-box");
-    const reviewTemplate = document.querySelector("#review-template");
-    const reviewCon = document.querySelector("#review-con");
-    const baseUrl = `https://search.imdbot.workers.dev/`
+    const movieDetailsTemplate = document.querySelector("#movie-details-template");
+    const movieDetailsCon = document.querySelector("#movie-details-con");
+    const baseUrl = "https://swapi.dev/api";
 
-    // make first AJAX call
-    function getMovies() {
-        fetch(`${baseUrl}?q=speed`)
-        .then(response => response.json())
-        .then(function(response) {
-            // console.log(response.description);
-            // store description array (list of movies in movies)
-            const movies = response.description;
-            const ul = document.createElement("ul");
+    // Make AJAX call to get Star Wars characters
+    function getCharacters() {
+        fetch(`${baseUrl}/people/`)
+            .then(response => response.json())
+            .then(function(response) {
+                const characters = response.results;
+                const ul = document.createElement("ul");
 
-            movies.forEach(movie => {
-                const li = document.createElement("li");
-                const a = document.createElement("a");
-                // console.log(movie['#TITLE']);
-                a.textContent = movie['#TITLE'];
-                a.dataset.review = movie['#IMDB_ID'];
-                li.appendChild(a);
-                ul.appendChild(li);
-            });
-            movieBox.appendChild(ul);
-        })
-        .then(function(){
-            const links = document.querySelectorAll("#movie-box li a");
-            links.forEach(link =>{
-                link.addEventListener("click", getReview);
+                characters.forEach(character => {
+                    const li = document.createElement("li");
+                    const a = document.createElement("a");
+                    a.textContent = character.name;
+                    a.dataset.movieUrl = character.films[0]; // Assuming the first film in the list
+                    li.appendChild(a);
+                    ul.appendChild(li);
+                });
+
+                movieBox.appendChild(ul);
+
+                const links = document.querySelectorAll("#movie-box li a");
+                links.forEach(link => {
+                    link.addEventListener("click", getMovieDetails);
+                });
             })
-        })
-        .catch(err => {
-            console.log(err);
-            // send message to user in DOM, there was an issue
-        });
+            .catch(err => {
+                console.log(err);
+                // Handle error - display a message to the user
+            });
     }
 
-    function getReview(e) {
-        // console.log(e.currentTarget.dataset.review);
-        // console.log(this.dataset.review);
-        const reviewID = e.currentTarget.dataset.review;
-        // https://search.imdbot.workers.dev/?tt=tt00111257
+    // Make AJAX call to get movie details when character link is clicked
+    function getMovieDetails(e) {
+        const movieUrl = e.currentTarget.dataset.movieUrl;
 
-        fetch(`${baseUrl}?tt=${reviewID}`)
-        .then(response =>response.json())
-        .then(function(response) {
-            // console.log(response.short.review.reviewBody);
-            // clear out reviewCon
-            reviewCon.innerHTML = "";
-            // Make a copy of the template
-            const template = document.importNode(reviewTemplate.content, true);
-            const reviewBody = template.querySelector(".review-description");
-            reviewBody.innerHTML = response.short.review.reviewBody;
-            reviewCon.appendChild(template);
-        })
-        .catch(error = (
-            console.log(error)
-            // add message to user that is written in the DOM
-        ));
+        fetch(movieUrl)
+            .then(response => response.json())
+            .then(function(response) {
+                const template = document.importNode(movieDetailsTemplate.content, true);
+                const movieTitle = template.querySelector(".movie-title");
+                const openingCrawl = template.querySelector(".opening-crawl");
+                const moviePoster = template.querySelector(".movie-poster");
+
+                movieTitle.textContent = response.title;
+                openingCrawl.textContent = response.opening_crawl;
+
+                // Add logic to get the movie poster URL from another source or use a placeholder
+
+                movieDetailsCon.innerHTML = "";
+                movieDetailsCon.appendChild(template);
+            })
+            .catch(error => {
+                console.log(error);
+                // Handle error - display a message to the user
+            });
     }
 
-    // call the function to load list
-    getMovies();
-    
-  
+    // Call the function to load Star Wars characters
+    getCharacters();
+
 })();
-
